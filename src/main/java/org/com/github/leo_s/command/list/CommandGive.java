fdc69c50025fd3efffb60ff2d0e3f732a77d9592
Leonardo <contactleonardosarica@gmail.com>
2022-12-09 11:55:03 -0600

    O

--- src/main/java/org/com/github/leo_s/command/list/CommandGive.java
+++ src/main/java/org/com/github/leo_s/command/list/CommandGive.java
@@ -1,12 +1,10 @@
 package org.com.github.leo_s.command.list;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFlag;
 import org.com.github.leo_s.command.ICommand;
-import org.com.github.leo_s.present.Present;
 
 import static org.com.github.leo_s.Christmas.config;
+import static org.com.github.leo_s.present.Present.givePresentItem;
 import static org.com.github.leo_s.utils.files.Convert.convert;
 
 @SuppressWarnings("ConstantConditions")
@@ -23,20 +21,7 @@ public class CommandGive implements ICommand {
             return;
         }
         if(args.length == 3) {
-            ItemFlag[] itemFlags = new ItemFlag[]{};
-            for(String flag : config.getStringList("present-item.flags")){
-                itemFlags = new ItemFlag[]{ItemFlag.valueOf(flag)};
-            }
-            try {
-                Integer.parseInt(args[2]);
-            }catch (NumberFormatException e){
-                player.sendMessage("§cThe amount must be a number!");
-                return;
-            }
-            new Present(
-                    config.getString("present-item.name"),
-                    Material.getMaterial(config.getString("present-item.material")),
-                    config.getStringList("present-item.lore"), itemFlags).givePresentItem(player, args[1], Integer.parseInt(args[2]));
+            givePresentItem(player, args[1], Integer.parseInt(args[2]));
         }else{
             player.sendMessage("§cUse: §6/present give <player> <amount>");
         }
--- src/main/java/org/com/github/leo_s/present/Present.java
+++ src/main/java/org/com/github/leo_s/present/Present.java
@@ -11,42 +11,34 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-import static org.bukkit.Bukkit.getServer;
 import static org.com.github.leo_s.Christmas.config;
+import static org.com.github.leo_s.utils.files.Convert.convert;
 
 @SuppressWarnings({"unused", "FieldMayBeFinal", "FieldCanBeLocal", "ConstantConditions", "DuplicatedCode"})
 public class Present {
 
-    public static HashMap<Player, Present> presentHashMap = new HashMap<>();
-    private String name;
-    private Material material;
-    private List<String> lore;
-    private ItemFlag[] flags;
-    String texture_head;
-
-    public Present(String name, Material material, List<String> lore,
-                   ItemFlag[] flags) {
-        this.name = name;
-        this.material = material;
-        this.lore = lore;
-        this.flags = flags;
-        this.texture_head = config.getString("present-item.texture");
-
-    }
-
-    public void givePresentItem(Player p, String target, int amount) {
+    public static void givePresentItem(Player p, String target, int amount) {
         Player targetPlayer = Bukkit.getPlayer(target);
         if(targetPlayer != null){
-            ItemStack item = new ItemStack(material, amount);
+            Material m = Material.valueOf(config.getString("present-item.material"));
+            String name = convert(config.getString("present-item.name"));
+            List<String> lore = config.getStringList("present-item.lore").stream().map(s -> s.replace("&", "§").replace("%player%", targetPlayer.getName())).toList();
+            String texture = config.getString("present-item.texture");
 
-            if(material == Material.PLAYER_HEAD){
+            ItemFlag[] itemFlags = new ItemFlag[]{};
+            for(String flag : config.getStringList("present-item.flags")){
+                itemFlags = new ItemFlag[]{ItemFlag.valueOf(flag)};
+            }
+
+            ItemStack item = new ItemStack(m, amount);
+
+            if(m == Material.PLAYER_HEAD){
                 SkullMeta skullMeta = (SkullMeta) item.getItemMeta();
                 GameProfile profile = new GameProfile(UUID.fromString("3b01e4c7-f07b-4cff-a744-88d7eae47249"), null);
-                profile.getProperties().put("textures", new Property("textures", texture_head));
+                profile.getProperties().put("textures", new Property("textures", texture));
                 try{
                     Field profileField = skullMeta.getClass().getDeclaredField("profile");
                     profileField.setAccessible(true);
@@ -54,14 +46,14 @@ public class Present {
                 } catch (NoSuchFieldException | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
-                skullMeta.setDisplayName(name.replace("&", "§"));
-                skullMeta.setLore(lore.stream().map(s -> s.replace("&", "§").replace("%player%", targetPlayer.getName())).toList());
+                skullMeta.setDisplayName(name);
+                skullMeta.setLore(lore);
                 item.setItemMeta(skullMeta);
             }else{
                 ItemMeta itemMeta = item.getItemMeta();
-                itemMeta.setDisplayName(name.replace("&", "§"));
-                itemMeta.setLore(lore.stream().map(s -> s.replace("&", "§").replace("%player%", targetPlayer.getName())).toList());
-                for(ItemFlag flag : flags){
+                itemMeta.setDisplayName(name);
+                itemMeta.setLore(lore);
+                for(ItemFlag flag : itemFlags){
                     itemMeta.addItemFlags(flag);
                 }
                 item.setItemMeta(itemMeta);
@@ -69,20 +61,9 @@ public class Present {
             if(targetPlayer.getInventory().firstEmpty() != -1) {
                 targetPlayer.getInventory().addItem(item);
                 targetPlayer.sendMessage("§aYou received a present from " + p.getName());
-                presentHashMap.put(targetPlayer, this);
             }else{
                 p.sendMessage("§cThe player " + targetPlayer.getName() + " does not have space in inventory!");
             }
         }
-    }
-
-    public String getName() {
-        return name;
-    }
-    public Material getMaterial() {
-        return material;
-    }
-    public List<String> getLore() {
-        return lore;
     }
 }
